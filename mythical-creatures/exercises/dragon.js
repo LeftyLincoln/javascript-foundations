9f9b0e3156559c9e072eed8e5bd252c815495f6e
Travis Parks <travisparks0788@gmail.com>
2022-12-05 20:54:48 -0500

    Add six passing tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name
+        this.rider = rider
+        this.hungry = true
+    
+    }
 
+    greet() {
+       return `Hi, ${this.rider}!`
+    
+    }
+
+    eat() {
+        if(this.hungry >= 3) {
+            return this.hungry
+        } else {
+            return false
+            }
+    }
+}
+
+module.exports = Dragon;
