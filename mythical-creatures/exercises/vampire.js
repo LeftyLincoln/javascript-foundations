ec098acd4648886acffb503b32b284882c6b38f0
Travis Parks <travisparks0788@gmail.com>
2022-12-07 17:36:57 -0500

    Add 8 passing answers

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -2,9 +2,22 @@
 
 
 class Vampire {
-    constructor(name) {
-        this.name = name
+    constructor(vampName, vampPet) {
+        this.name = vampName,
+        this.pet = vampPet || 'bat'
+        this.thirsty = true
+        this.blood = 0
+        this.ouncesDrank = 0
     }
+
+    drink() {
+        this.blood += 1
+        if (this.blood > 0) {
+            return this.thirsty = false
+        }
+    }
+
+
 }
 
 
