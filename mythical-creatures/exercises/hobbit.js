2f53cc49bb8e7176dd6de9fc6b7f46bd2e5f0560
Travis Parks <travisparks0788@gmail.com>
2022-12-11 10:32:48 -0500

    Add final answers

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -5,6 +5,7 @@ class Hobbit {
       this.age = 0
       this.adult = false
       this.old = false
+      this.hasRing = true
     }
 
     celebrateBirthday() {
@@ -15,7 +16,16 @@ class Hobbit {
         if (this.age >= 101) {
           this.old = true    
         }
+    }
 
+    getRing() {
+      if (this.name === 'Frodo') {
+        this.hasRing = true
+        return 'Here is the ring!'
+      } else {
+        this.hasRing = false
+        return 'You can\'t have it!'
+      }
     }
 }
 
