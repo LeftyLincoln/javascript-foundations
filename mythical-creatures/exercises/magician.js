ec57a0ec7fb96f283498bc228f62a579796c9242
Travis Parks <travisparks0788@gmail.com>
2022-12-13 10:43:38 -0500

    add answers

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -4,14 +4,25 @@ class Magician {
         this.name = `The Great ${name}`
         this.assistant = assistant
         this.favoriteAccessory = clothing || 'top hat'  
+        this.confidencePercentage = 10
     }
 
     performIncantation(spell) {
         return `${spell.toUpperCase()}!`
     }
 
-    performTrick(spells) {
-        return spells
+    performTrick() {
+        this.confidencePercentage += 10
+        if (this.favoriteAccessory === 'cape') {
+            return 'PULL DOVE FROM SLEEVE'
+        } else {
+            return 'PULL RABBIT FROM TOP HAT'
+          }
+    }
+
+    performShowStopper()  {
+        if ()          
+        return 'WOW! The magician totally just sawed that person in half!'
     }
 
 
