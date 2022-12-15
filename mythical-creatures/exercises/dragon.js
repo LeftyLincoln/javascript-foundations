d84c0ba3a3fc61e52bf87d815c1349609c51cbac
Travis Parks <travisparks0788@gmail.com>
2022-12-15 09:34:13 -0500

    redo testing

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,26 +1,26 @@
-class Dragon {
-    constructor(name, rider) {
-        this.name = name
-        this.rider = rider
-        this.hungry = true
-        this.food = 0
-    }
+class Dragon{
+constructor(name, rider) {
+     this.name = name
+     this.rider = rider
+     this.hungry = true
+     this.food = 0
+}
 
-    greet() {
-       return `Hi, ${this.rider}!`
-    
-    }
+greet()  {
+    return `Hi, ${this.rider}!`
+}
 
-    eat() {
-        this.food += 1
-        if(this.food < 3) {
-        return this.hungry = true
-        } return this.hungry = false
-    } 
+eat() {
+ this.food += 1
+ if (this.food >= 3) {
+    return this.hungry = false
+ } return this.hungry = true
+}
 
-} 
-    
-    
+}
 
 
 module.exports = Dragon;
+
+
+
