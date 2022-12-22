5c7277ba8722fd9ec2cd2948526009c88308b28b
Travis Parks <travisparks0788@gmail.com>
2022-12-22 18:53:10 -0500

    almost

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -5,7 +5,26 @@ class Direwolf {
         this.name = name
         this.home = home || 'Beyond the Wall'
         this.size = size || 'Massive'
+        this.starksToProtect = []
+        this.huntsWhiteWalkers = true
+        
     }
+    protect(lord) {
+        // console.log(lord)
+        if (this.home === lord.location && this.starksToProtect.length <= 1) {
+        this.starksToProtect.push(lord)
+        lord.safe = true
+        this.huntsWhiteWalkers = false
+        }
+    }
+    leave(lord) {
+        if (lord.safe) {
+            this.starksToProtect.pop()
+            lord.safe = false
+        }
+    }
+
+
 }
 
 
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,8 +1,27 @@
+const Direwolf = require("./direwolf")
+
 class Stark {
     constructor(lord) {
         this.name = lord.name
         this.location = lord.area || 'Winterfell'
+        this.safe = false
     }
+
+    sayHouseWords() {
+        if (this.safe) {
+            return 'The North Remembers'
+        } else {
+            return 'Winter is Coming'
+        }
 }
+    callDirewolf(name, location) {
+        new Direwolf
+        
+    }
+
+    
+}
+
+
 
 module.exports = Stark;
