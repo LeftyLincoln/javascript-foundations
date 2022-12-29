f693bf46e50a3514c8dc90f0a4fa6fadf73cab16
Travis Parks <travisparks0788@gmail.com>
2022-12-29 10:47:32 -0500

    feat finished golf and golf course

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,24 @@
 class GolfCourse {
+    constructor(course, challenge, spots, attractions) {
+        this.name = course
+        this.difficulty = challenge
+        this.openings = spots
+        this.features = attractions
+        this.currentlyPlaying = []   
+    }
+    checkInGroup(group) {
+        console.log(group)
 
-
+        if (group.length <= this.openings) {
+            for (var i = 0; i < group.length; i ++) {
+                this.currentlyPlaying.unshift(group[i].name)
+            }
+            this.openings -= group.length
+            return 'You\'re checked in. Have fun!'
+        } else {
+            return 'Sorry, we are currently booked! Please come back later.'
+        }
+    }
 }
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,53 @@
 class Golfer {
+    constructor(golfObject) {
+        // console.log(golfObject)
+        this.name = golfObject.name
+        this.handicap = golfObject.handicap
+        this.frustration = 0
+        this.confidence = 0
+    }
 
+    calculateAvg(score) {
+        return `I usually shoot a ${score + this.handicap} on average.`
+    }
 
+    playRound(golfCourse) {
+        if (golfCourse.difficulty === 'hard') {
+            this.frustration += 500
+        } else {
+            this.frustration += 100
+        }
+    }
+   
+    hitTheRange() {
+        this.confidence += 10
+    }
+
+    marvel(golfCourse) {
+        // console.log(golfCourse)
+        return `I love the ${golfCourse.features[0]} and ${golfCourse.features[1]} on this course!`
+    }
+
+    whatYaShoot(holeScore) {
+        if (holeScore === 4) {
+            this.frustration = 20
+            return 'Doh!'
+        } else if (holeScore === 0) {
+            this.frustration = 10
+            return 'Booyah!'
+        } else if (holeScore === 2) {
+            this.frustration = 30
+            return 'Doh!'
+        } else if (holeScore === 3) {
+            this.frustration = 20
+            return 'Doh!'
+        } else if (holeScore === -1 || -2) {
+            this.frustration = 0
+            return 'I AM THE GREATEST GOLFER ALIVE!'
+        }
+    }
 }
+
+
 
 module.exports = Golfer;
