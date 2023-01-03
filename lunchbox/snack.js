4904c4be8f97ba2fe1b6de2377c73a920f305a2f
Travis Parks <travisparks0788@gmail.com>
2023-01-02 21:02:21 -0500

    feat passing tests

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,19 @@
 class Snack {
+    constructor(snack) {
+        console.log(snack)
+        this.deliciousLevel = "extra"
+        this.type = snack
+        this.amount = 100
+        this.isInLunchBox = false
+        this.cuttingItClose = false
+    }
 
+    getEaten() {
+        this.amount -= 10
+        if (this.amount <= 20) {
+            this.cuttingItClose = true
+        }
+     }
 }
 
 module.exports = Snack;
