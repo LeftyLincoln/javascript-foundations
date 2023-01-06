b5b08548d0f55731724b5b43fa4f0c63309e74b2
Travis Parks <travisparks0788@gmail.com>
2023-01-06 09:37:31 -0500

    finish skier tests

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,4 +1,22 @@
 class Skier {
+  constructor(name) {
+    this.name = name
+    this.hasLiftTicket = true
+    this.skillLevel = 1
+    this.nextSlope = 'green circle'
+  }
+
+  takeLesson() {
+    this.skillLevel += 1
+  }
+
+  pickSlope() {
+    if (this.skillLevel === 3) {
+      this.nextSlope ='blue square'
+    } else if (this.skillLevel >= 5) {
+      this.nextSlope = 'black diamond'
+    }
+  }
 
 }
 
