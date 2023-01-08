da23036a3b6a4d6f9c3162fbb27793eed93d3dba
Travis Parks <travisparks0788@gmail.com>
2023-01-07 19:03:59 -0500

    Finish all tests

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,33 @@
 class Roadrace {
+  constructor(object) {
+    console.log(object);
+    this.name = object.title
+    this.location = object.city
+    this.distance = null
+    this.participants = []
+  }
 
+  setDistance(length) {
+    this.distance = length
+    return `The ${this.name} in ${this.location} is a ${length} mile race.`  }
+
+  registerParticipants(object) {
+    // console.log(object);
+    this.participants.push(object)
+
+   
+    
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].completedRaces.push(this.name)
+      // this.participants[i].milesRun += this.distance
+      this.participants[i].runRace(this.name, this.distance)
+    }
+
+  }
 }
+
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,4 +1,28 @@
 class Runner {
+  constructor(name, age) {
+  this.name = name 
+  this.age = age
+  this.milesRun = 0
+  this.fitness = 25
+  this.completedRaces = []
+  }
+
+  runSomeMiles(amount) {
+  this.milesRun += amount
+  this.fitness += amount
+  }
+
+  stretch() {
+    this.fitness += .5
+  }
+
+  runRace(name, length) {    
+    this.completedRaces.push(name)
+    this.runSomeMiles(length)
+    
+  }
+
+
 
 }
 
