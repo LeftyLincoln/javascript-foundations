d7e1656b01c225725a306ec74f0b9d19a2115a9f
Travis Parks <travisparks0788@gmail.com>
2023-01-09 10:59:53 -0500

    partial completion

--- recipes/src/ingredient.js
+++ recipes/src/ingredient.js
@@ -1,5 +1,11 @@
 class Ingredient {
-  
+  constructor(object) {
+    this.name = object.name
+    this.amount = object.amount
+  }
+  changeAmount(number) {
+    this.amount = number
+  }
 }
 
 module.exports = Ingredient;
--- recipes/src/recipe.js
+++ recipes/src/recipe.js
@@ -1,4 +1,29 @@
 class Recipe {
+  constructor(object) {
+    console.log(object);
+    this.name = object.name
+    this.ingredients = object.ingredients
+    this.attempted = false
+    this.rating = null
+  }
+
+rateRecipe(number) {
+  this.attempted = true
+  this.rating = number
+}
+
+changeIngredientAmount(ingredient, newAmount) {
+  for (var i = 0; i < this.ingredients.length; i++) {
+    if (this.ingredients[i].name === ingredient) {
+    this.ingredients[i].amount = newAmount
+    }
+  }
+}
+
+generateGroceryList() {
+ 
+}
+
 
 }
 
